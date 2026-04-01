735cb456049c525acbdc0361673dd558952b60b9
joshckidd <josh@larameekidd.com>
2026-04-01 11:14:36 -0400

    search by image

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,15 +1,45 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from lib.semantic_search import *
+import json
 
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents, model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+        self.documents = documents
+        self.texts = []
+        for doc in documents:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
 
     def embed_image(self, image_path):
         img = Image.open(image_path)
         return self.model.encode(img)
     
+    def search_with_image(self, image_path):
+        embedding = self.embed_image(image_path)
+        i = 0
+        results = []
+        for te in self.text_embeddings:
+            score = cosine_similarity(te, embedding)
+            results.append({"score": score, "document": self.documents[i]})
+            i += 1
+        
+        results = sorted(results, key=lambda x:x["score"], reverse=True)
+
+        return results[:5]
+
+    
 def verify_image_embedding(image_path):
     search = MultimodalSearch()
     embedding = search.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def image_search_command(image_path):
+    with open("data/movies.json", "r") as f:
+        movies = json.load(f)
+        
+    documents = movies["movies"]    
+    search = MultimodalSearch(documents)
+
+    return search.search_with_image(image_path)
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -5,14 +5,25 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Multimodal Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    normalize_parser = subparsers.add_parser("verify_image_embedding", help="Verify an image embedding")
-    normalize_parser.add_argument("image_path", type=str, help="Image path")
+    verify_image_embedding_parser = subparsers.add_parser("verify_image_embedding", help="Verify an image embedding")
+    verify_image_embedding_parser.add_argument("image_path", type=str, help="Image path")
+
+    image_search_parser = subparsers.add_parser("image_search", help="Search by image")
+    image_search_parser.add_argument("image_path", type=str, help="Image path")
 
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             verify_image_embedding(args.image_path)
+        case "image_search":
+            results = image_search_command(args.image_path)
+            i = 1
+            for result in results:
+                print(f"{i}. {result["document"]["title"]} (similarity: {result["score"]:.3f})")
+                print(f"   {result["document"]["description"][:80]}...")
+                print("")
+                i += 1
 
 if __name__ == "__main__":
     main()
